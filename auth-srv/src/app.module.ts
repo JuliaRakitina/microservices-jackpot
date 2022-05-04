c280e6caf30a84858d86ef26099391a7aa194756
Julia Rakitina <julia.rakitina@gmail.com>
2022-05-04 23:29:23 +0400

    Auth service set up

--- auth-srv/src/app.module.ts
+++ auth-srv/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5454,
+      database: 'micro_auth',
+      username: 'postgres',
+      password: 'postgres',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ auth-srv/src/auth/auth.entity.ts
@@ -0,0 +1,19 @@
+import { Exclude } from 'class-transformer';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Auth extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column({ type: 'varchar' })
+  public email!: string;
+
+  @Exclude()
+  @Column({ type: 'varchar' })
+  public password!: string;
+
+  @Exclude()
+  @Column({ type: 'varchar' })
+  public role!: string;
+}
